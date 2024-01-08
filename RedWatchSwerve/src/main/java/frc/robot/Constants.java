09fbb6dd2d3cd050b864cda53ca0efe261bdfa17
PraveenPrabaharan <83364745+PraveenPrabaharan@users.noreply.github.com>
2024-01-07 20:14:38 -0500

    imported the vender dep for pigeon2

--- RedWatchSwerve/src/main/java/frc/robot/Constants.java
+++ RedWatchSwerve/src/main/java/frc/robot/Constants.java
@@ -122,4 +122,6 @@ public final class Constants {
     public static final int kDriverControllerPort = 0;
     public static final double kDriveDeadband = 0.05;
   }
+public static final int kPigeonIMUId = 0;
+public static final String kCANivoreCANBusName = "canivore";
 }
